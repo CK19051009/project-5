21cbb204a03717bfe1abe02bf0faf903016bb11d
DinhKiet <dinhkiet2k4@gmail.com>
2024-09-03 06:54:57 +0700

    third commit

--- src/app/(pages)/logout/page.tsx
+++ src/app/(pages)/logout/page.tsx
@@ -5,7 +5,7 @@ import { signOut } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function logoutPage () {
+export default function LogoutPage () {
     
     const router = useRouter();
 
